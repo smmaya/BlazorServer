ee2b3bddc3dd599491ab4514bb33562a4662a3df
Sławomir Majchrzak <smmaya@yahoo.fr>
2022-09-05 11:00:55 +0200

    Blazor first steps

---
+++ BlazorServer/Data/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BlazorServer.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int IndexNumber { get; set; }
+
+    }
+}
+
